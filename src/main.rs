2cc8ae14cd92fbdf7f6e1accd6cfe17d63e77d79
Jon Pacheco <jon.pacheco2@gmail.com>
2024-05-05 17:59:39 +0100

    feat: get rtic + usb keyboard working

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,17 @@
 #![no_std]
 #![no_main]
 
-use defmt::*;
-use defmt_rtt as _;
-use panic_probe as _;
-
-use rp2040_hal as hal;
-
-use hal::{
-    clocks::{init_clocks_and_plls, Clock},
-    pac::{self, interrupt},
-    sio::Sio,
-    watchdog::Watchdog,
-};
-
-use usb_device::{class_prelude::*, prelude::*};
-use usbd_hid::{
-    descriptor::{generator_prelude::*, KeyboardReport},
-    hid_class::HIDClass,
-};
-
 #[link_section = ".boot2"]
 #[used]
 pub static BOOT2: [u8; 256] = rp2040_boot2::BOOT_LOADER_W25Q080;
 
 #[rtic::app(device = rp2040_hal::pac, peripherals = true, dispatchers = [I2C0_IRQ])]
 mod app {
-    use embedded_hal::digital::{InputPin, OutputPin};
+    use defmt::*;
+    use defmt_rtt as _;
+    use panic_probe as _;
+
+    use embedded_hal::digital::OutputPin;
     use rp2040_hal as hal;
 
     use hal::{
@@ -37,21 +22,33 @@ mod app {
         Sio, Watchdog,
     };
 
+    use usb_device::{class_prelude::*, prelude::*};
+    use usbd_hid::{
+        descriptor::{generator_prelude::*, AsInputReport, KeyboardReport},
+        hid_class::HIDClass,
+    };
+
     const XOSC_CRYSTAL_FREQ: u32 = 12_000_000u32;
 
     #[shared]
     struct Shared {
         led: gpio::Pin<gpio::bank0::Gpio25, gpio::FunctionSioOutput, gpio::PullNone>,
+        usb_device: UsbDevice<'static, rp2040_hal::usb::UsbBus>,
+        hid_keyboard: HIDClass<'static, rp2040_hal::usb::UsbBus>,
     }
 
     #[local]
-    struct Local {}
+    struct Local {
+        key_report: KeyboardReport,
+    }
 
     #[monotonic(binds = TIMER_IRQ_0, default = true)]
     type MyMono = Monotonic<Alarm0>;
 
     #[init]
     fn init(c: init::Context) -> (Shared, Local, init::Monotonics) {
+        info!("Initialising app");
+
         // Soft-reset doesn't release hardware spinlocks
         // Release them here to avoid a deadlock after debug or watchdog reset
         unsafe {
@@ -72,6 +69,7 @@ mod app {
         .ok()
         .unwrap();
 
+        let mut timer = hal::Timer::new(c.device.TIMER, &mut resets, &clocks);
         let sio = Sio::new(c.device.SIO);
         let pins = gpio::Pins::new(
             c.device.IO_BANK0,
@@ -80,30 +78,94 @@ mod app {
             &mut resets,
         );
 
+        // Setup the USB driver
+        // NOTE: The USB bus allocator needs a static lifetime so we can store references in shared struct
+        static mut USB_BUS_ALLOCATOR: Option<UsbBusAllocator<rp2040_hal::usb::UsbBus>> = None;
+        let usb_bus_allocator = unsafe {
+            USB_BUS_ALLOCATOR = Some(UsbBusAllocator::new(hal::usb::UsbBus::new(
+                c.device.USBCTRL_REGS,
+                c.device.USBCTRL_DPRAM,
+                clocks.usb_clock,
+                true,
+                &mut resets,
+            )));
+
+            USB_BUS_ALLOCATOR.as_ref().unwrap()
+        };
+
+        // Setup the HID class driver, providing keyboard reports
+        let hid_keyboard = HIDClass::new(&usb_bus_allocator, KeyboardReport::desc(), 1);
+
+        // Setup the USB device description
+        let usb_device = UsbDeviceBuilder::new(&usb_bus_allocator, UsbVidPid(0x1404, 0x1404))
+            .strings(&[StringDescriptors::default()
+                .manufacturer("Cloverpad")
+                .product("Cloverpad MX")
+                .serial_number("CLP-MX-01")])
+            .unwrap()
+            .build();
+
         let mut led = pins.gpio25.reconfigure();
         led.set_low().unwrap();
 
-        let mut timer = hal::Timer::new(c.device.TIMER, &mut resets, &clocks);
         let alarm = timer.alarm_0().unwrap();
-        blink_led::spawn_after(500.millis()).unwrap();
+        toggle_key::spawn_after(500.millis()).unwrap();
 
         (
-            Shared { led },
-            Local {},
+            Shared {
+                led,
+                usb_device,
+                hid_keyboard,
+            },
+            Local {
+                key_report: KeyboardReport::default(),
+            },
             init::Monotonics(Monotonic::new(timer, alarm)),
         )
     }
 
-    #[task(shared = [led], local = [tog: bool = true])]
-    fn blink_led(mut c: blink_led::Context) {
+    #[task(binds = USBCTRL_IRQ, shared = [usb_device, hid_keyboard])]
+    fn poll_usb(c: poll_usb::Context) {
+        let poll_usb::SharedResources {
+            usb_device,
+            hid_keyboard,
+        } = c.shared;
+
+        (usb_device, hid_keyboard).lock(|dev, hid| {
+            if dev.poll(&mut [hid]) {
+                // The OS may send a report to the keypad, e.g. setting NumLock LED
+                // We don't need to process this, so read + discard it
+                let mut throwaway = [0; 64];
+                let _ = hid.pull_raw_output(&mut throwaway);
+            }
+        })
+    }
+
+    #[task(shared = [led, hid_keyboard], local = [key_report, tog: bool = true])]
+    fn toggle_key(mut c: toggle_key::Context) {
         if *c.local.tog {
             c.shared.led.lock(|l| l.set_high().unwrap());
+            c.local.key_report.keycodes[0] = 0x04;
         } else {
             c.shared.led.lock(|l| l.set_low().unwrap());
+            c.local.key_report.keycodes[0] = 0x00;
         }
+
+        c.shared.hid_keyboard.lock(|h| {
+            push_hid_report(h, c.local.key_report)
+                .ok()
+                .unwrap_or_default()
+        });
 
         *c.local.tog = !*c.local.tog;
 
-        blink_led::spawn_after(500.millis()).unwrap();
+        toggle_key::spawn_after(500.millis()).unwrap();
+    }
+
+    fn push_hid_report<U: usb_device::bus::UsbBus, IR: AsInputReport>(
+        hid: &mut HIDClass<U>,
+        report: &IR,
+    ) -> Result<usize, usb_device::UsbError> {
+        hid.push_input(report)
     }
 }
